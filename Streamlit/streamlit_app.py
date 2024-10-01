fe222bcaa8b9a718787858f56d819efba046c1c7
Declan Costello <79241861+dec1costello@users.noreply.github.com>
2024-09-30 22:03:16 -0600

    Update streamlit_app.py

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -96,7 +96,7 @@ p.yaxis.axis_label_text_font_size = '18pt'
 p.legend.title = 'Player'
 p.title.text_font_size = '25pt'
 p.legend.label_text_font_size = '8pt'
-p.legend.location = "top_right"  # Change the location to top_right
+p.legend.location = "top_left"  # Change the location to top
 p.legend.orientation = "vertical"  # Change the orientation to vertical
 p.legend.click_policy = "hide"
 
