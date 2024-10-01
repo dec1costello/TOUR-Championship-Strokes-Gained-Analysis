dabd3a83f75af69d86ac3159f8fab7c83a164e35
Declan Costello <79241861+dec1costello@users.noreply.github.com>
2024-09-30 22:41:22 -0600

    Update streamlit_app.py

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -29,12 +29,13 @@ st.sidebar.info("Read more about my code on my [Github](https://github.com/dec1c
 st.title("Player Performance")
 condensed_df = pd.read_csv('Streamlit/Rolling_SG_group_by_hole_player.csv')
 
-player1 = 'Mickelson'
-player2=player1
-player1 = st.selectbox("Select Golfer 1", condensed_df['last_name'].unique(),placeholder="Select Golfer 1...")
-player2 = st.selectbox("Select Golfer 2", condensed_df['last_name'].unique(),placeholder="Select Golfer 2...")
+# player1 = 'Mickelson'
 
-condensed_df = condensed_df[(condensed_df['last_name'] == player1) | (condensed_df['last_name'] == player2)]
+player1 = st.selectbox("Select Golfer 1", condensed_df['last_name'].unique(), placeholder="Select Golfer 1...")
+player2 = st.selectbox("Select Golfer 2", condensed_df['last_name'].unique(), placeholder="Select Golfer 2...")
+
+# Filter the DataFrame based on the selected golfers
+condensed_df_filtered = condensed_df[(condensed_df['last_name'] == player1) | (condensed_df['last_name'] == player2)]
 
 # st.dataframe(condensed_df) 
 
