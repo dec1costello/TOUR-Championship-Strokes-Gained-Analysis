e039e7581899177d3e928e54639cade5f9cadb0d
Declan Costello <79241861+dec1costello@users.noreply.github.com>
2024-09-30 21:20:43 -0600

    Update streamlit_app.py

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -30,12 +30,7 @@ st.title("Player Performance")
 tab_faq = st.tabs(["tab_ppredictor", "FAQ"])
 
 with tab_ppredictor:
-    
-    x = [1, 2, 3, 4, 5]
-    y = [6, 7, 2, 4, 5]
-    p = figure(title="simple line example", x_axis_label="x", y_axis_label="y")
-    p.line(x, y, legend_label="Trend", line_width=2)
-    st.bokeh_chart(p, use_container_width=True)
+    st.markdown(" ### Frequently Asked Questions 🔎 ")
 
 with tab_faq:
     
