ca9d367643bd9088e0a3bbc291331d07f96a3427
Declan Costello <79241861+dec1costello@users.noreply.github.com>
2024-09-30 22:32:01 -0600

    Update streamlit_app.py

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -35,9 +35,8 @@ condensed_df = pd.read_csv('Streamlit/Rolling_SG_group_by_hole_player.csv')
 
 player1 = st.selectbox("Select Golfer 1", condensed_df['last_name'].unique(),placeholder="Select Golfer 1...")
 # player2 = st.selectbox("Select Golfer 2", condensed_df['last_name'].unique(),placeholder="Select Golfer 2...")
-condensed_df = condensed_df[condensed_df['last_name'].isin([player1])]
 
-# condensed_df = condensed_df[condensed_df['last_name'] == player]
+condensed_df = condensed_df[condensed_df['last_name'] == player]
 # st.dataframe(condensed_df) 
 
 
