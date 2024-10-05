b9666b77f2f5bb5dcdc015f184a9028b2b1c4adc
Declan Costello <79241861+dec1costello@users.noreply.github.com>
2024-10-05 13:13:25 -0600

    Update streamlit_app.py

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -286,7 +286,7 @@ with tab_faqs:
                     st.markdown("""
                     ___
                     ### Consider listening to these podcasts too!
-                    <iframe style="border-radius:12px" src="https://open.spotify.com/episode/0jKnJG9pSCNsNsOsoEwNrh?si=f6759e5ccae24142?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    <iframe style="border-radius:12px" src="https://open.spotify.com/episode/4aE0DrfBNLgKMna5oBiLlM?si=06b6bb876dd74c57?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                     <br><br>
                     """, unsafe_allow_html=True)
 
