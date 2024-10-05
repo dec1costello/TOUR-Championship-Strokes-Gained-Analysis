df070db855bf5d731e8d4c55d854ecd0dbb1cfb6
Declan Costello <79241861+dec1costello@users.noreply.github.com>
2024-10-04 21:28:10 -0600

    Update streamlit_app.py

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -38,7 +38,7 @@ profile_tab, Comparisons_tab, tab_faqs = st.tabs(["Profiles", "Comparisons", "FA
 with profile_tab:
     col1, col2 = st.columns(2)
     with col1:
-        golfer = st.selectbox("Select Golfer 1", condensed_df['last_name'].unique(), index=16)
+        golfer = st.selectbox("Select Player", condensed_df['last_name'].unique(), index=16)
         # Define the specific order for the 'SG_bins' categories
     df = df[df['last_name'] == golfer]
     order = ['OTT', '200+', '200-150', '150-100', '100-50', '50-0', 'Putting']
