b34729e64c9534e005dbdf6a40fbe6bc1d39b1f7
Declan Costello <79241861+dec1costello@users.noreply.github.com>
2024-10-06 15:17:14 -0600

    Update streamlit_app.py

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -15,7 +15,7 @@ st.set_page_config(
     initial_sidebar_state="expanded")
 
 st.sidebar.image('https://github.com/dec1costello/Golf/assets/79241861/0f9673d0-36c6-4d6f-928b-34d171a19350')
-st.sidebar.markdown(" ## About")
+st.sidebar.markdown(" ## ⛳ About")
 st.sidebar.markdown("This Dashboard offers deeper insights into a golfer's true abilities during the 2011 TOUR Championship. The primary aspiration is to contribute meaningful insights to the golf community."  )  
 st.sidebar.info("Read more about my code on my [Github](https://github.com/dec1costello/TOUR-Championship-Strokes-Gained-Analysis).", icon="ℹ️")
 condensed_df = pd.read_csv('Streamlit/Rolling_SG_group_by_hole_player.csv')
@@ -36,7 +36,7 @@ with profile_tab:
         st.balloons()
     df = df[df['last_name'] == golfer]
 
-    st.header("Total SG by Shot Type")
+    st.header("🏌️ Total SG by Shot Type")
 
     description_text_2 = """
      The plot directly below displays a players's 
@@ -168,7 +168,7 @@ with profile_tab:
     p3.title.text_font_size = '18pt'
     st.bokeh_chart(p3, use_container_width=True)
 
-    st.header("SG Percentile by Shot Type")
+    st.header("⛳ SG Percentile by Shot Type")
     description_text_3 = """
     By looking at a players's SG Percentile, 
     we can see where he consistently underperformed in each shot type bin, 
