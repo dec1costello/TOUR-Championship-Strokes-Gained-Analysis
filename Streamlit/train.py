3414c205852bae5a22d8f7d8934ff538d184b6a9
Declan Costello <79241861+dec1costello@users.noreply.github.com>
2024-10-09 22:19:13 -0600

    Update train.py

--- Streamlit/train.py
+++ Streamlit/train.py
@@ -1,107 +1,84 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import skops.io as sio
-from sklearn.compose import ColumnTransformer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_selection import SelectKBest, chi2
-from sklearn.impute import SimpleImputer
-from sklearn.metrics import (
-    ConfusionMatrixDisplay,
-    accuracy_score,
-    confusion_matrix,
-    f1_score,
-)
+import pandas as pd 
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler, OrdinalEncoder
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+# Set random seed
+seed = 42
 
-# loading the data
-bank_df = pd.read_csv("Streamlit/train.csv", index_col="id", nrows=1000)
-bank_df = bank_df.drop(["CustomerId", "Surname"], axis=1)
-bank_df = bank_df.sample(frac=1)
+################################
+########## DATA PREP ##########
+################################
 
+# Load in the data
+df = pd.read_csv("wine_quality.csv")
 
-# Splitting data into training and testing sets
-X = bank_df.drop(["Exited"], axis=1)
-y = bank_df.Exited
+# Split into train and test sections
+y = df.pop("quality")
+X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random_state=seed)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=125
-)
+#################################
+########## MODELLING ############
+#################################
 
-# Identify numerical and categorical columns
-cat_col = [1, 2]
-num_col = [0, 3, 4, 5, 6, 7, 8, 9]
+# Fit a model on the train section
+regr = RandomForestRegressor(max_depth=2, random_state=seed)
+regr.fit(X_train, y_train)
 
-# Transformers for numerical data
-numerical_transformer = Pipeline(
-    steps=[("imputer", SimpleImputer(strategy="mean")), ("scaler", MinMaxScaler())]
-)
+# Report training set score
+train_score = regr.score(X_train, y_train) * 100
+# Report test set score
+test_score = regr.score(X_test, y_test) * 100
 
-# Transformers for categorical data
-categorical_transformer = Pipeline(
-    steps=[
-        ("imputer", SimpleImputer(strategy="most_frequent")),
-        ("encoder", OrdinalEncoder()),
-    ]
-)
-
-# Combine pipelines using ColumnTransformer
-preproc_pipe = ColumnTransformer(
-    transformers=[
-        ("num", numerical_transformer, num_col),
-        ("cat", categorical_transformer, cat_col),
-    ],
-    remainder="passthrough",
-)
-
-# Selecting the best features
-KBest = SelectKBest(chi2, k="all")
-
-# Random Forest Classifier
-model = RandomForestClassifier(n_estimators=100, random_state=125)
-
-# KBest and model pipeline
-train_pipe = Pipeline(
-    steps=[
-        ("KBest", KBest),
-        ("RFmodel", model),
-    ]
-)
-
-# Combining the preprocessing and training pipelines
-complete_pipe = Pipeline(
-    steps=[
-        ("preprocessor", preproc_pipe),
-        ("train", train_pipe),
-    ]
-)
-
-# running the complete pipeline
-complete_pipe.fit(X_train, y_train)
+# Write scores to a file
+with open("metrics.txt", 'w') as outfile:
+        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
+        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
 
 
-## Model Evaluation
-predictions = complete_pipe.predict(X_test)
-accuracy = accuracy_score(y_test, predictions)
-f1 = f1_score(y_test, predictions, average="macro")
+##########################################
+##### PLOT FEATURE IMPORTANCE ############
+##########################################
+# Calculate feature importance in random forest
+importances = regr.feature_importances_
+labels = df.columns
+feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
+feature_df = feature_df.sort_values(by='importance', ascending=False,)
 
-print("Accuracy:", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1, 2))
+# image formatting
+axis_fs = 18 #fontsize
+title_fs = 22 #fontsize
+sns.set(style="whitegrid")
+
+ax = sns.barplot(x="importance", y="feature", data=feature_df)
+ax.set_xlabel('Importance',fontsize = axis_fs) 
+ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
+ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
+
+plt.tight_layout()
+plt.savefig("feature_importance.png",dpi=120) 
+plt.close()
 
 
-## Confusion Matrix Plot
-predictions = complete_pipe.predict(X_test)
-cm = confusion_matrix(y_test, predictions, labels=complete_pipe.classes_)
-disp = ConfusionMatrixDisplay(
-    confusion_matrix=cm, display_labels=complete_pipe.classes_
-)
-disp.plot()
-plt.savefig("model_results.png", dpi=120)
+##########################################
+############ PLOT RESIDUALS  #############
+##########################################
 
-## Write metrics to file
-with open("metrics.txt", "w") as outfile:
-    outfile.write(f"\nAccuracy = {round(accuracy, 2)}, F1 Score = {round(f1, 2)}\n\n")
+y_pred = regr.predict(X_test) + np.random.normal(0,0.25,len(y_test))
+y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
+res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
 
+ax = sns.scatterplot(x="true", y="pred",data=res_df)
+ax.set_aspect('equal')
+ax.set_xlabel('True wine quality',fontsize = axis_fs) 
+ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
+ax.set_title('Residuals', fontsize = title_fs)
 
-# saving the pipeline
-sio.dump(complete_pipe, "bank_pipeline.skops")
+# Make it pretty- square aspect ratio
+ax.plot([1, 10], [1, 10], 'black', linewidth=1)
+plt.ylim((2.5,8.5))
+plt.xlim((2.5,8.5))
+
+plt.tight_layout()
+plt.savefig("residuals.png",dpi=120) 
