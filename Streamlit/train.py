f8ae76288ce8e9ebaa103c6f294cc11355c7178d
Declan Costello <79241861+dec1costello@users.noreply.github.com>
2024-10-09 22:25:15 -0600

    Update train.py

--- Streamlit/train.py
+++ Streamlit/train.py
@@ -12,7 +12,7 @@ seed = 42
 ################################
 
 # Load in the data
-df = pd.read_csv("wine_quality.csv")
+df = pd.read_csv("Streamlit/wine_quality.csv")
 
 # Split into train and test sections
 y = df.pop("quality")
