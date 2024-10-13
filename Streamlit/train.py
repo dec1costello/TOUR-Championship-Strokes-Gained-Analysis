f3a0475b3f1be501175cc124072330c6fdf65355
Declan Costello <79241861+dec1costello@users.noreply.github.com>
2024-10-13 12:34:47 -0600

    Update train.py

--- Streamlit/train.py
+++ Streamlit/train.py
@@ -33,7 +33,7 @@ train_score = regr.score(X_train, y_train) * 100
 test_score = regr.score(X_test, y_test) * 100
 
 # Write scores to a file
-with open("Streamlit/results.txt", 'w') as outfile:
+with open("Streamlit/Training\ Report/results.txt", 'w') as outfile:
         outfile.write("Training variance explained: %2.1f%%\n" % train_score)
         outfile.write("Test variance explained: %2.1f%%\n" % test_score)
 
@@ -57,7 +57,8 @@ ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
 ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
 
 plt.tight_layout()
-plt.savefig("Streamlit/feature_importance.png",dpi=120) 
+# plt.savefig("Streamlit/feature_importance.png",dpi=120) 
+plt.savefig("Streamlit/Training\ Report/feature_importance.png", dpi=120)
 plt.close()
 
 
@@ -81,4 +82,4 @@ plt.ylim((2.5,8.5))
 plt.xlim((2.5,8.5))
 
 plt.tight_layout()
-plt.savefig("Streamlit/residuals.png",dpi=120) 
+plt.savefig("Streamlit/Training\ Report/residuals.png",dpi=120) 
